0a88642353f3b28c6ce3019af37d051bfa812a28
mansi <mansipatil.0068@gmail.com>
2024-05-16 00:31:15 +0530

    Initial commit

---
+++ sum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class sum{
+    
+    int input(int n)
+    {
+        int a[n][n], flg[n][n];
+        int i=0, j=0, k=n-1;
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                flg[i][j]=0;
+            }
+        }
+        while(i<n*n)
+        {
+            
+        }
+    }
+};
+
+int main()
+{
+    
+}
